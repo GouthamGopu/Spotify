81f6c5e31af4af0604ad52d54149a40d213622eb
Gopu.Goutham <160241716+GouthamGopu@users.noreply.github.com>
2024-09-10 21:58:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,34 +60,59 @@ let playlists=[
     link:"https://open.spotify.com/playlist/37i9dQZF1DX2zAr9vdmFlU"
   }
 ];
-let card=document.querySelector(".spotify-playlist-cards");
-let spotifyPlaylist=document.querySelector(".spotify-playlist");
-let showAll=document.querySelector(".spotify-playlist-show-all");
+document.addEventListener("DOMContentLoaded", () => {
+  let card = document.querySelector(".spotify-playlist-cards");
+  let spotifyPlaylist = document.querySelector(".spotify-playlist");
+  let showAll = document.querySelector(".spotify-playlist-show-all");
 
-playlists.slice(0,5).forEach((playlist)=>{
-    addToPlaylist(playlist.image,playlist.title,playlist.discription);
-});
+  let isShowingAll = false; // Track whether all playlists are shown or not
 
-spotifyPlaylist.addEventListener("click",()=>{
-  card.innerHTML=``;
-  playlists.forEach((playlist)=>{
-      addToPlaylist(playlist.image,playlist.title,playlist.discription);
+  // Function to render playlists
+  function addToPlaylist(image, title, discription) {
+      card.innerHTML += `
+          <div class="card flex justify-center">
+              <img class="pic" src="${image}">
+              <span class="title block">${title}</span>
+              <span class="discription block">${discription}</span>
+              <img class="play-button" src="svgs/song-play.svg" alt="play">
+          </div>`;
+  }
+
+  // Initial load of playlists (show the first 5)
+  playlists.slice(0, 5).forEach((playlist) => {
+      addToPlaylist(playlist.image, playlist.title, playlist.discription);
   });
-});
 
-showAll.addEventListener("click",()=>{
-  card.innerHTML=``;
-  playlists.forEach((playlist)=>{
-      addToPlaylist(playlist.image,playlist.title,playlist.discription);
+  // Event listener for "Spotify Playlist"
+  spotifyPlaylist.addEventListener("click", () => {
+      card.innerHTML = ``;
+      playlists.forEach((playlist) => {
+          addToPlaylist(playlist.image, playlist.title, playlist.discription);
+      });
+      showAll.textContent = "Show Less"; // Change button text to "Show Less"
+      isShowingAll = true; // Update the state
   });
-});
 
-function addToPlaylist(image,title,discription){
-  card.innerHTML+=`
-    <div class="card flex justify-center">
-    <img class="pic" src="${image}">
-    <span class="title block">${title}</span>
-    <span class="discription block">${discription}</span>
-    <img class="play-button" src="svgs/song-play.svg" alt="play">
-    </div>`
-}
+  // Event listener for "Show All / Show Less"
+  showAll.addEventListener("click", () => {
+      if (isShowingAll) {
+          // Show only the first 5 playlists
+          card.innerHTML = ``;
+          playlists.slice(0, 5).forEach((playlist) => {
+              addToPlaylist(playlist.image, playlist.title, playlist.discription);
+          });
+          showAll.textContent = "Show All"; // Change button text to "Show All"
+      } else {
+          // Show all playlists
+          card.innerHTML = ``;
+          playlists.forEach((playlist) => {
+              addToPlaylist(playlist.image, playlist.title, playlist.discription);
+          });
+          showAll.textContent = "Show Less"; // Change button text to "Show Less"
+      }
+      isShowingAll = !isShowingAll; // Toggle the state
+  });
+
+  // Debugging
+  console.log('Script loaded and DOM content is ready.');
+});
